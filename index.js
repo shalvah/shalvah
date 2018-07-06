771adbb55cc13323f0015083af36e29e84a2a9be
shalvah <diakon.ng@gmail.com>
2018-07-06 23:25:17 +0100

    Add email

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ program.version('0.1.0')
     .description('Shalvah in your command-line')
     .parse(process.argv);
 
-inquirer.prompt([
-    {
-        name: 'link',
-        type: 'list',
-        message: shalvah.bio + '\n' + shalvah.prompt,
-        choices: shalvah.links
-    }
-]).then(answers => {
+console.log(shalvah.bio);
+inquirer.prompt({
+    name: 'link',
+    type: 'list',
+    message: shalvah.prompt,
+    choices: shalvah.links.concat({
+        'name': `...Or shoot me an email (${shalvah.email})`,
+        'value': 'mailto:' + shalvah.email
+    })
+}).then(answers => {
     console.log(`Opening ${answers.link}`);
     opn(answers.link);
 });
